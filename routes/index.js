144b7fe26547401752a277841e689260f4b33702
sz8386pr <sz8386pr@gmail.com>
2018-04-25 12:19:24 -0500

    getting recipes from allrecipes in progress TODO: redirect to different page TODO: make better interface

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', isLoggedIn, function(req, res, next) {
-    res.render('index', { user: req.user });
-});
-
-module.exports = router;
-
+var Allrecipes = require('../services/scrape_allrecipes');
+var ar_scrape = Allrecipes.scrape;
+var ar_get_recipe = Allrecipes.get_recipe;
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
@@ -17,3 +12,39 @@ function isLoggedIn(req, res, next) {
 		res.redirect('/auth/login')
 	}
 }
+
+/* GET home page. */
+router.get('/', isLoggedIn, function(req, res, next) {
+    res.render('index', { user: req.user });
+});
+
+// POST home page
+router.post('/', isLoggedIn, function(req, res, next) {
+	let site = req.body.site;
+	let keyword = req.body.keyword;
+	let page = req.body.page;
+
+	// If it's coming from the search page, page value will be set to 1
+	if (page === undefined) {
+		page = '1'
+	}
+	console.log(site, keyword, page);
+
+
+	if (site === 'allrecipes') {
+
+		ar_get_recipe(function(err, recipes){
+			if (err){
+				res.render('error') // TODO render error page
+			} else {
+				res.render('external_recipes', {recipes: recipes});
+			}
+		}, keyword, page);
+	}
+	else {
+		res.render('index')
+	}
+
+});
+
+module.exports = router;
--- services/scrape_allrecipes.js
+++ services/scrape_allrecipes.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-let url1 = 'https://www.allrecipes.com/recipe/255344/3-ingredient-cookies/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202';
 
+// scrape from the specific recipe page
 let scrape = async (url) =>
     axios.get(url)
         .then((response) => {
@@ -56,12 +56,62 @@ let scrape = async (url) =>
 
             }
         },
-    (error) => console.log(err)
+    (error) => console.log(error)
 );
 
-// test
-scrape(url1)
-    .then((value) => {
-    console.log(value); // Success!
-});
+// finds recipe title/link
+function get_recipe(callback, keyword, page) {
+	process.nextTick(function(){
+		const base_url = 'https://www.allrecipes.com/search/results/?wt=';
+		let url = base_url.concat(keyword, '&page=', page);
+		console.log(url);
+		axios.get(url)
+			.then((response) => {
+					if(response.status === 200) {
+						const html = response.data;
+						const $ = cheerio.load(html);
 
+						// get the recipe title & link
+						let titles = [];
+						$('.fixed-recipe-card__title-link').each(function() {
+							let link = $(this).attr('href');
+							let title = $(this).text().trim();
+							if (link !== undefined) {
+								titles.push({'title': title, 'link': link});
+							}
+						});
+
+						// get the rating and round to 1 decimal place
+						let ratings = [];
+						$('.fixed-recipe-card__ratings span').each(function() {
+							let rating = $(this).attr('data-ratingstars');
+							if (rating !== undefined)
+							ratings.push({'rating': parseFloat(rating).toFixed(1)})
+						});
+
+						// merge the arrays of dictionaries together
+						let recipes = [];
+						for (var i = 0; titles.length > i; i++){
+							recipes.push(Object.assign({}, titles[i], ratings[i]));
+						}
+						console.log(recipes);   // debug
+						callback (null, recipes)
+					}
+				},
+				(error) => callback(error)
+			);
+	});
+}
+
+// // test
+// let url1 = 'https://www.allrecipes.com/recipe/255344/3-ingredient-cookies/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202';
+// scrape(url1)
+//     .then((value) => {
+//     console.log(value); // Success!
+//     });
+
+
+module.exports = {
+	get_recipe: get_recipe,
+	scrape: scrape,
+};
