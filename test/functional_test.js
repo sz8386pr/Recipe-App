99376dc3beb6dc5b9ba52d93e241158d80be288d
sz8386pr <sz8386pr@gmail.com>
2018-05-08 10:14:45 -0500

    Added more tests and additional comments for the tests

--- test/functional_test.js
+++ test/functional_test.js
@@ -69,7 +69,7 @@ describe('empty test db before tests, and and close db after ', () => {
 			})
 	});
 
-	beforeEach('get collections and delete all dos', function(done) {
+	beforeEach('connect to the test db server', function(done) {
 
 		mongodb_client.connect(test_db_url)
 			.then((client) => {
@@ -83,9 +83,9 @@ describe('empty test db before tests, and and close db after ', () => {
 			.then(() => { done() })
 	});
 
-	describe('signup/login', function() {
+	describe('signup/login test', function() {
 
-		it('should connect to server', function(done) {
+		it('should be able to connect to server', function(done) {
 			chai_server
 				.get('/')
 				.end((err, res) => {
@@ -96,6 +96,7 @@ describe('empty test db before tests, and and close db after ', () => {
 		});
 
 		it('should be able to create account', function(done) {
+			this.timeout(5000);     // on rare occasion this task takes little longer than the default timeout--2000ms. Presumably due to the delay to connect to mLab
 			cookie_server
 				.post('/auth/signup/')
 				.send({username: 'testuser', password: 'testuser', confirm_password: 'testuser', email: 'testuser@email.com'})
@@ -115,8 +116,30 @@ describe('empty test db before tests, and and close db after ', () => {
 
 		});
 
-		it('should be able to login', function(done) {
-			chai.request(server)
+		it('should NOT be able to login with a wrong username', function(done) {
+			cookie_server
+				.post('/auth/login')
+				.send({username: 'badusername', password: 'testuser'})
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.text).to.include('Username not found');
+					done();
+				})
+		});
+
+		it('should NOT be able to login with a wrong password', function(done) {
+			cookie_server
+				.post('/auth/login')
+				.send({username: 'testuser', password: 'badpassword'})
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.text).to.include('Password incorrect');
+					done();
+				})
+		});
+
+		it('should be able to login with the right combination', function(done) {
+			cookie_server
 				.post('/auth/login')
 				.send({username: 'testuser', password: 'testuser'})
 				.end((err, res) => {
@@ -127,15 +150,8 @@ describe('empty test db before tests, and and close db after ', () => {
 		})
 	});
 
-	describe('external search', function() {
-		// beforeEach('login', function(done) {
-		// 	cookie_server
-		// 		.post('/auth/login')
-		// 		.send({username: 'testuser', password: 'testuser'})
-		// 		.then((err, res) => {
-		// 			done()
-		// 		})
-		// });
+
+	describe('external search test', function() {
 
 		it('should be able to open external search page', function(done) {
 			cookie_server
@@ -145,23 +161,22 @@ describe('empty test db before tests, and and close db after ', () => {
 					expect(res.text).to.include('Search recipes from other sources');
 					done();
 				})
-
-
 		});
 
 		it('should be able to search Allrecipes with keyword chicken', function(done) {
+			this.timeout(5000);     // on rare occasion this task takes little longer than the default timeout--2000ms. Presumably due to the delay to connect to mLab
 			cookie_server
 				.post('/recipe/external-search/allrecipes')
 				.send({site: 'allrecipes', keyword: 'chicken'})
 				.end((err, res) => {
 					expect(res.status).to.equal(200);
-					expect(res.text).to.include('Search result for' || 'ALLRECIPES');
+					expect(res.text).to.include('Search result for' && 'CHICKEN' && 'ALLRECIPES');
 					done()
 				})
 		});
 
-		it('should save chicken-parmasan recipe from ALLrecipes', function(done) {
-			this.timeout(5000);     // this task takes little longer than the default timeout--2000ms
+		it('should be able to save chicken-parmasan recipe from ALLrecipes', function(done) {
+			this.timeout(10000);     // this task takes longer than the default timeout--2000ms
 			cookie_server
 				.post('/recipe/save')
 				.send({save_url: 'https://www.allrecipes.com/recipe/223042/chicken-parmesan/'})
@@ -173,7 +188,8 @@ describe('empty test db before tests, and and close db after ', () => {
 		})
 	});
 
-	describe('create recipe', function() {
+
+	describe('create recipe test', function() {
 
 		it('should render create recipe page', function(done) {
 			cookie_server
@@ -185,7 +201,7 @@ describe('empty test db before tests, and and close db after ', () => {
 				})
 		});
 
-		it('should create a new recipe', function(done) {
+		it('should be able to create a new recipe', function(done) {
 			cookie_server
 				.post('/recipe/create')
 				.send({title:'test recipe', category: 'test category', description: 'test desc', duration_hour: 1,
@@ -193,10 +209,61 @@ describe('empty test db before tests, and and close db after ', () => {
 				.end((err, res) => {
 					expect(res.status).to.equal(200);
 					expect(res.text).to.include('test recipe' && 'chop chop');
+					recipes.find().toArray().then( (recipes) => {
+						expect(recipes.length).to.equal(2);
+						var recipe = recipes[1];
+						expect(recipe).to.have.property('title').equal('test recipe');
+						done();
+					});
+				})
+		})
+	});
+
+
+	describe('internal search & publish function test', function() {
+
+		it('should not find anything on search because nothing was published', function(done) {
+			cookie_server
+				.post('/recipe/search')
+				.send({keyword: 'recipe'})
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.text).to.include('There are no recipes that matches');
+					recipes.find().toArray().then( (recipes) => {
+						expect(recipes.length).to.equal(2);
+						var recipe = recipes[1];
+						expect(recipe).to.have.property('title').equal('test recipe');
+						expect(recipe).to.have.property('published').equal(false);  // recipes created have published on false by default
+						done();
+					});
+				})
+		});
+
+		it('publish test recipe', function(done) {
+			cookie_server
+				.post('/recipe/publish/test recipe')
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					recipes.find().toArray().then( (recipes) => {
+						expect(recipes.length).to.equal(2);
+						var recipe = recipes[1];
+						expect(recipe).to.have.property('title').equal('test recipe');
+						expect(recipe).to.have.property('published').equal(true);   // it is published now
+						done();
+					});
+				})
+		});
+
+		it('should display test recipe on search result this time', function(done) {
+			this.timeout(5000);     // on rare occasion this task takes little longer than the default timeout--2000ms. Presumably due to the delay to connect to mLab
+			cookie_server
+				.post('/recipe/search')
+				.send({keyword: 'test recipe'})
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.text).to.include('test recipe' && 'by testuser');    // asset that search finds published recipes
 					done()
 				})
 		})
-
-
-	});
+	})
 });
